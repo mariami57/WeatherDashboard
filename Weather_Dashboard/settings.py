3ef33bf2f5b17ba3fc582fd40c1222e5958ff4b7
mariami57 <mmitova3@gmail.com>
2025-09-05 09:29:49 +0300

    added security settings and env file

--- Weather_Dashboard/settings.py
+++ Weather_Dashboard/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
+ENV = config("DJANGO_ENV", default="dev")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +23,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5)(4t=zxl%89%*x56_uo*76q)^=n!x#vd2t0y)42%dwra#$$-m'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if ENV == "dev":
+    DEBUG = config ("DEBUG", default=True, cast=bool)
+    ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="").split(",")
+else:
+    DEBUG = False
+    ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
 
-ALLOWED_HOSTS = []
+CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS").split(",")
 
 
 # Application definition
@@ -73,10 +81,15 @@ WSGI_APPLICATION = 'Weather_Dashboard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': config("DB_HOST"),
+        'PORT': config("DB_PORT"),
     }
 }
 
