7b2ab9ae2223536d38e360ab7000c49ddd8b1dc1
mariami57 <mmitova3@gmail.com>
2025-09-08 18:42:38 +0300

    updated city suggestions view

--- weather/views.py
+++ weather/views.py
@@ -1,7 +1,14 @@
+import csv
+from idlelib import query
+from pathlib import Path
+
 import requests
 from django.conf import settings
 from django.http import JsonResponse
+from unidecode import unidecode
+
 from Weather_Dashboard.utils import get_weather
+
 
 
 # Create your views here.
@@ -17,17 +24,32 @@ def weather_api(request):
     else:
         return JsonResponse({'error': 'Could not fetch weather'}, status=500)
 
+
+DATA_PATH = Path(__file__).resolve().parent / 'data' / 'worldcities.csv'
+CITIES = []
+with open(DATA_PATH, encoding='utf-8') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        CITIES.append({
+            'city': row['city'],
+            'country': row['country'],
+            'search_name': unidecode(row['city'].lower()),
+        })
+
 def city_suggestions(request):
-    query = request.GET.get('q', '')
+    query = request.GET.get("q", "").strip().lower()
+
     if not query:
         return JsonResponse([], safe=False)
-    url = (
-        f"http://api.openweathermap.org/geo/1.0/direct?q={query}&limit=5&appid={settings.API_KEY}"
-    )
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        results = [f"{city['name']}, {city['country']}" for city in data]
-        return JsonResponse(results, safe=False)
-    else:
-        return JsonResponse([], safe=False)
+
+    q_norm = unidecode(query).lower()
+    results = [
+        {
+            'name': c['city'],
+            'country':c['country'],
+            'display': f"{c['city']}, {c['country']}"
+        }
+        for c in CITIES if q_norm in c['search_name'].lower()
+    ][:15]
+
+    return JsonResponse(results, safe=False)
