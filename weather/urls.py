d7ad9b466cc7fab8d8174e6d4093a8ef7436c16e
mariami57 <mmitova3@gmail.com>
2025-09-08 21:03:35 +0300

    added 7 days forecast logic

--- weather/urls.py
+++ weather/urls.py
@@ -5,4 +5,5 @@ from weather import views
 urlpatterns = [
     path("api/cities/", views.city_suggestions, name="city_suggestions"),
     path("api/weather/", views.weather_api, name="weather_api"),
+    path("api/onecall/", views.onecall_forecast, name="onecall_forecast"),
 ]
--- weather/views.py
+++ weather/views.py
@@ -14,8 +14,15 @@ from Weather_Dashboard.utils import get_weather
 # Create your views here.
 def weather_api(request):
     city = request.GET.get('city')
-    if not city:
-        return JsonResponse({'error': 'No city provided'}, status=400)
+    lat = request.GET.get("lat")
+    lon = request.GET.get("lon")
+
+    if lat and lon:
+        url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={settings.API_KEY}&units=metric"
+    elif city:
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={settings.API_KEY}&units=metric"
+    else:
+        return JsonResponse({"error": "Missing parameters"}, status=400)
 
     data = get_weather(city)
 
@@ -33,6 +40,8 @@ with open(DATA_PATH, encoding='utf-8') as f:
         CITIES.append({
             'city': row['city'],
             'country': row['country'],
+            'lat': row['lat'],
+            'lon': row['lng'],
             'search_name': unidecode(row['city'].lower()),
         })
 
@@ -47,9 +56,28 @@ def city_suggestions(request):
         {
             'name': c['city'],
             'country':c['country'],
+            'lat': row['lat'],
+            'lon': row['lng'],
             'display': f"{c['city']}, {c['country']}"
         }
         for c in CITIES if q_norm in c['search_name'].lower()
     ][:15]
 
     return JsonResponse(results, safe=False)
+
+def onecall_forecast(request):
+    lat = request.GET.get("lat")
+    lon = request.GET.get("lon")
+
+    if not lat or not lon:
+        return JsonResponse({"error": "Missing lat/lon parameters"}, status=400)
+
+    url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude=minutely,hourly,alerts&units=metric&appid={settings.API_KEY}"
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        return JsonResponse(data)
+    except requests.RequestException as e:
+        return JsonResponse({"error": f"Could not fetch weather: {e}"}, status=500)
