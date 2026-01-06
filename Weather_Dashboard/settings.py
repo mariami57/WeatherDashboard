8d5455c42a0df7e11f53677966c62bec74936818
mariami57 <mmitova3@gmail.com>
2026-01-06 12:54:47 +0200

    switched to SQLite

--- Weather_Dashboard/settings.py
+++ Weather_Dashboard/settings.py
@@ -94,13 +94,9 @@ WSGI_APPLICATION = 'Weather_Dashboard.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config("DB_NAME"),
-        'USER': config("DB_USER"),
-        'PASSWORD': config("DB_PASSWORD"),
-        'HOST': config("DB_HOST"),
-        'PORT': config("DB_PORT"),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
